59de44f00daa4e19485a293af13f136c88a1fff8
Pro-Mish <mishackmadubandlela@gmail.com>
2025-09-19 03:42:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ load_dotenv()
 
 app = Flask(__name__)
 #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mish_writes:Admin@localhost:5432/poetrydb"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mish_writes:Admin@localhost:5432/poetrydb"
+app.config["SQLALCHEMY_DATABASE_URL"] = "postgresql://mish_writes_user:XjGdvfxtRj0zuAaMHz1IMqWEPPfdXUyS@dpg-d350o5er433s738e5tt0-a/mish_writes"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["MAX_CONTENT_LENGTH"] = 2 * 3024 * 3024  # 2MB
